a5148707e209710d81790e10ed351ed1581359a8
iki1uc <entertrainer@web.de>
2026-07-01 04:35:14 +0200

    Add AXINXA object with attach method

--- axinxa.js
+++ axinxa.js
@@ -1,1 +1,21 @@
+// axinxa.js (Minimal-Version)
+
+const AXINXA = {
+  attach(MXU) {
+    const axis = document.createElement('div');
+    axis.style = `
+      width:140px;
+      height:60px;
+      background:#555;
+      color:#fff;
+      margin-top:10px;
+      display:flex;
+      align-items:center;
+      justify-content:center;
+      font-family:Arial;
+    `;
+    axis.innerText = "AXINXA ACTIVE";
+    document.body.appendChild(axis);
+  }
+};
 
